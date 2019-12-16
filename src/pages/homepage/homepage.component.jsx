c176dfaf3dcdb0056fc8774f99d8a83f13ec5955
Depala <jdepala@deloitte.com>
2019-12-16 23:56:04 +0530

    added menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Components
+    import Directory from '../../components/directory/directory.component';
+
+// Styling 
+    import './homepage.styles.scss';
+
+const HomePage = () => {
+
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+};
+
+export default HomePage;
+
