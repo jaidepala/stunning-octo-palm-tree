5c84978e4b433954a68b4486e0059b4739c9e051
Depala <jdepala@deloitte.com>
2019-12-24 02:43:02 +0530

    added persistor to index.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,29 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// import sessionStorage from 'redix-persist/lib/storage';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: [
+        /*
+            !   Ref
+            *    
+            *   Since user is handled by 
+            *   firebase, we do not 
+            *    include it.
+        */
+        'cart'
+    ]
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
